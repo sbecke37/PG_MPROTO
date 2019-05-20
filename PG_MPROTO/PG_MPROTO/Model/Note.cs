7cda555ce17a0f6e31838e1b448945a538bb8004
shawnb <shawnb@MXL8291ZHB>
2019-05-20 15:35:48 -0500

    commit

--- PG_MPROTO/PG_MPROTO/Model/Note.cs
+++ PG_MPROTO/PG_MPROTO/Model/Note.cs
@@ -1,11 +1,10 @@
-﻿using AppServiceHelpers.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PG_MPROTO.Model
 {
-    public class Message : EntityData
+    public class Message 
     {
 
 
